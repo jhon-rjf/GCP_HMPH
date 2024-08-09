a4094aa27731570bd7252e945e21ad4162afbef0
poppingstar <71365717+poppingstar@users.noreply.github.com>
2024-08-09 17:44:43 +0900

    Update pub.py

--- pub.py
+++ pub.py
@@ -16,25 +16,32 @@ class video_processor:
 
   def encode_current_frame(self):
     _,img=self.capture.read()
-    encoded_img=base64.b64decode(img)
+    encoded_img=base64.b64encode(img)
     return encoded_img
   
-  def skip_video_per_sec(self, skip_time_msec) -> None:
-    skip_time_sec=skip_time_msec*1000
+  def skip_video_per_sec(self, skip_time_sec) -> None:
+    skip_time_msec=skip_time_sec*1000
     current_position=cv2.CAP_PROP_POS_MSEC
     current_position_msec=self.capture.get(current_position)
-    next_position=current_position_msec+skip_time_sec
+    next_position=current_position_msec+skip_time_msec
     self.capture.set(current_position, next_position)
+    self.check_video_overrun(next_position)
+  
+  def check_video_overrun(self, next_position):
+    total_frames=int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps=self.capture.get(cv2.CAP_PROP_FPS)
+    video_length_msec=(total_frames/fps)*1000
+    
+    if (video_length_msec<next_position):
+      self.capture.set()
 
 class publisher:
-  def __init__(self, project, topic, certificate_key) -> None: 
+  def __init__(self, project, topic) -> None: 
     self.publisher=pubsub_v1.PublisherClient()
     self.topic_path=publisher.topic_path(project, topic)
 
-    
-
   def publish(self, product) -> None:
-    publisher.publish(self.topic_path, product)
+    self.publisher.publish(self.topic_path, product)
 
 video_path=input('please input path of video file: ')
 project='andong-24-team-102'
