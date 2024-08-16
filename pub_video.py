f90ac5a0651683ebcca1793239eb4a2e05387149
poppingstar <71365717+poppingstar@users.noreply.github.com>
2024-08-16 13:38:09 +0900

    Update pub_video.py

--- pub_video.py
+++ pub_video.py
@@ -3,20 +3,17 @@ import base64
 import cv2
 from google.cloud import pubsub_v1
 
-class video_processor:
+class Video_processor:
   def __init__(self) -> None:
     self.video_path=input('please input path of video file: ')
     self.capture=cv2.VideoCapture(self.video_path)
     self._check_video_path()
+    self.capture.set(cv2.CAP_PROP_POS_AVI_RATIO, 1)
+    self.video_length=self.capture.get(cv2.CAP_PROP_POS_MSEC)
+    self.capture.set(cv2.CAP_PROP_POS_AVI_RATIO, 0)
 
   def encode_current_frame(self) -> None:
-    read, img_nparr=self.capture.read()
-    imread_false=not read
-
-    if imread_false:
-      self._video_restart()
-      _, img_nparr=self.capture.read()
-
+    _, img_nparr=self.capture.read()
     _, img=cv2.imencode('.png', img_nparr)
     img_byte=img.tobytes()
     encoded_img=base64.b64encode(img_byte)
@@ -26,7 +23,12 @@ class video_processor:
     skip_time_msec=skip_time_sec*1000
     current_position_msec=self.capture.get(cv2.CAP_PROP_POS_MSEC)
     next_position=current_position_msec+skip_time_msec
-    self.capture.set(cv2.CAP_PROP_POS_MSEC, next_position)
+    overrun=next_position>self.video_length
+
+    if overrun:
+      self._video_restart()
+    else:
+      self.capture.set(cv2.CAP_PROP_POS_MSEC, next_position)
 
   def _video_restart(self):
     self.capture.set(cv2.CAP_PROP_POS_AVI_RATIO, 0)
@@ -41,7 +43,7 @@ class video_processor:
       self.capture=cv2.VideoCapture(self.video_path)
       correct_path = self.capture.isOpened()
 
-class publisher:
+class Ppublisher:
   def __init__(self, topic_id) -> None: 
     self.publisher=pubsub_v1.PublisherClient()
     self.topic_path=topic_id
@@ -55,24 +57,16 @@ class publisher:
       print(f"Failed to publish: {e}")
       exit()    
 
-topic_id='projects/andong-24-team-102/topics/test'
-processor=video_processor()
-pub=publisher(topic_id)
+def main():
+  topic_id='projects/andong-24-team-102/topics/test'
+  processor=Video_processor()
+  pub=Ppublisher(topic_id)
 
-while True:
-  encoded_img=processor.encode_current_frame()
-  pub.publish(encoded_img)
-  time.sleep(1)
-  processor.skip_video_per_sec(1)
+  while True:
+    encoded_img=processor.encode_current_frame()
+    pub.publish(encoded_img)
+    time.sleep(1)
+    processor.skip_video_per_sec(1)
 
-
-'''
-    self._check_video_overrun()
-
-  def _check_video_overrun(self):
-    current_ratio=self.capture.get(cv2.CAP_PROP_POS_AVI_RATIO)
-    overrun=current_ratio>1
-
-    if overrun:
-      self._video_restart()
-'''
+if __name__=='__main__':
+  main()
